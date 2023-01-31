dfb37d72e991a0d4ef7856664085893ef1865544
jiwon615 <seg615@naver.com>
2023-01-31 21:40:26 +0900

    feat: 같은숫자는싫어by deque

--- src/algorithm/stack_queue/programmers/L1_같은숫자는싫어.java
+++ src/algorithm/stack_queue/programmers/L1_같은숫자는싫어.java
@@ -3,10 +3,10 @@ package algorithm.stack_queue.programmers;
 import java.util.*;
 
 /**
- * stack사용!
- * stack에 배열을 loop 돌며 하나 씩 넣음
+ * deque 사용!
+ * deque 배열을 loop 돌며 하나 씩 넣음
  * 넣기 전 직전에 들어간 것과 돌이하면 넣지 않음
- * loop 끝나면 stack에 들어있는 수 순서대로 출력
+ * loop 끝나면 deque에 들어있는 수 순서대로 출력
  */
 public class L1_같은숫자는싫어 {
 
